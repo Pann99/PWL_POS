950300e0fb0e84e8f4d5db7cf1ee123aebc3c301
= <=>
2025-02-25 14:45:43 +0700

    modifikasi migrate

---
+++ database/migrations/2025_02_25_074405_create_m_kategori_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('m_kategori', function (Blueprint $table) {
+            $table->id(); // Primary key (auto increment)
+            $table->string('nama', 100); // Nama kategori
+            $table->text('deskripsi')->nullable(); // Deskripsi kategori
+            $table->timestamps(); // Kolom created_at dan updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('m_kategori');
+    }
+};
